2d18ca4122995ba1bfe3546092c9e17649c917a9
Nelson Sandoval <techednelson@gmail.com>
2022-08-25 13:17:55 +0300

    initial commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
+
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  // deploy the contract
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
